f4dd5769ab147184c4fb0e804a63c16dfebee400
marcolino-t <167018680+marcolino-t@users.noreply.github.com>
2026-08-24 19:43:33 -0300

    commit

---
+++ 8 - periodo SIS/pagamento-services/src/main/java/com/microservicos/pagamento_services/PagamentoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.microservicos.pagamento_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoServicesApplication.class, args);
+	}
+
+}
